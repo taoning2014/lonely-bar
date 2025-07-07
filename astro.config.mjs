8b2ae0a49cd4728f83abb86d1d861f6eb9b448d6
Tao Ning <ningtaous@gmail.com>
2025-07-07 04:02:50 -0500

    Add first island in the Greeting component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://glowing-chaja-bca8b9.netlify.app/"
+  site: "https://glowing-chaja-bca8b9.netlify.app/",
+  integrations: [preact()]
 });
